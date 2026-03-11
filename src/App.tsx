f13cec14ac46b980e3c8d75925e1383a8309af4d
Jonatas-lopess <jonataslopes.011@gmail.com>
2026-03-11 19:10:44 -0300

    refactor: remove unused tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { useSyncBridge } from "./hooks/useSyncBridge";
 import { useDb } from "./db/context";
 
 import { Route, Switch } from "wouter";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
-import { queryClient } from "./lib/queryClient";
 import NotFound from "@/pages/not-found";
 import Dashboard from "@/pages/dashboard";
 import ClientsPage from "@/pages/clients";
@@ -37,11 +35,9 @@ export default function App({ hubIp, isTauri }: AppProps) {
   useSyncBridge(db, wsUrl, isTauri);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Router />
+    </TooltipProvider>
   );
 }
