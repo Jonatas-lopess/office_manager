4d1271a7a6f3903c17d7813331bc3916e68f777e
Jonatas-lopess <jonataslopes.011@gmail.com>
2026-03-18 15:47:47 -0300

    fix(db): updated the journal and generated the 0003 migration

---
+++ src/db/migrations/0003_fancy_franklin_richards.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `payments` (
+	`id` text PRIMARY KEY NOT NULL,
+	`service_id` text DEFAULT '' NOT NULL,
+	`payment_date` integer DEFAULT (strftime('%s', 'now') * 1000) NOT NULL,
+	`payment_method` text DEFAULT 'In_Cash' NOT NULL,
+	`amount` real DEFAULT 0 NOT NULL,
+	`created_at` integer DEFAULT (strftime('%s', 'now') * 1000) NOT NULL,
+	`updated_at` integer DEFAULT (strftime('%s', 'now') * 1000) NOT NULL
+);
+--> statement-breakpoint
+CREATE INDEX `service_id_idx` ON `payments` (`service_id`);--> statement-breakpoint
+ALTER TABLE `clients` ADD `has_serious_illness` integer DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE `services` DROP COLUMN `payment_date`;
